ce6bd6654afddc73e64b5aae10e5a7bf2a9c29d3
weyk <weyk@nifty.com>
2024-08-03 14:54:04 +0900

    initial commit.

---
+++ rollup.config.mjs
@@ -0,0 +1,13 @@
+import json from '@rollup/plugin-json'
+
+export default {
+  input: './.build/extension.mjs',
+  output: {
+    file: './lib/extension.js',
+    format: "cjs",
+    exports: "named",
+    sourcemap: true,
+  },
+  plugins: [json()],
+  external: ['vscode']
+}
